1b3b14cba950a301ae4ac59b5ff2d4dd739a80f4
Vadim Mirvald <mirvald.vadim@icloud.com>
2025-05-14 18:20:15 +0200

    Обновлены стили в компоненте Container: изменены отступы для улучшения визуального восприятия и адаптивности.

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export function Container({ children, className = "" }: ContainerProps) {
   return (
-    <div className={`flex flex-col max-w-[462px] mx-auto gap-[20px] lg:p-[0px] p-[20px] ${className}`}>
+    <div className={`flex flex-col max-w-[462px] mx-auto gap-[20px] lg:px-[0px] py-[20px] px-[20px] ${className}`}>
       {children}
     </div>
   );
